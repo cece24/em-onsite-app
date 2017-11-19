09cd0516377c772168e9f0addd228fd7ff4aa041
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-18 19:01:42 -0500

    Define pages#index to make get request for cece-onsite sessions, using open API. Display each session name, description and track

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,24 @@
+require "json"
+
 class PagesController < ApplicationController
   def index
-    #code
+    # base_uri "https://api.eventmobi.com/v2/events/10278f43-4e8b-44f9-94ee-35cdd4d7c6d3"
+    # response = HTTParty.get('https://experience.eventmobi.com/organization/eventmobi-support/event/21555/companies')
+    response = HTTParty.get("https://api.eventmobi.com/v2/events/10278f43-4e8b-44f9-94ee-35cdd4d7c6d3/sessions/resources", headers: {
+        "X-API-Key" => "b809f96a3b16f08f40cd0c59847c5497d2c31255c42ab11951d273a2d4cc6c51",
+        "Content-Type" => "application/json"
+      })
+
+    if response.code == 200
+      puts "Successful request!"
+      puts "Response received: #{response}"
+      parsed_data = JSON.parse(response.body)
+      @sessions = parsed_data["data"]
+      puts "Selected data now looks like: #{@sessions}"
+    else
+      puts "Request error: #{response.code}"
+    end
+
   end
+
 end
