5a3e754539460af6205657065a7c5c99599184e0
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-26 17:59:04 -0500

    Style contact support modal window

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,10 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
   console.log('js is werkin');
 
   var contactSupportLink = document.querySelector(".contact-support");
+  var contactModal = document.querySelector(".modal-container");
+  var closeModal = document.querySelector(".contact-close")
 
   contactSupportLink.addEventListener('click', function(e) {
     e.preventDefault();
 
-  })
+    contactModal.style.display = "unset";
+
+    closeModal.addEventListener('click', function() {
+      contactModal.style.display = "none";
+    });
+  });
 
 });
