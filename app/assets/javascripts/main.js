fc6de12c74a234e8257e1f240a6f3ed25e70aa27
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-26 17:42:50 -0500

    Add main link to contact support. Create contact support modal window in layout. Add styles

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('js is werkin');
+
+  var contactSupportLink = document.querySelector(".contact-support");
+
+  contactSupportLink.addEventListener('click', function(e) {
+    e.preventDefault();
+
+  })
+
+});
