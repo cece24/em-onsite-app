3a551edb324f72e6353c229e34c0943f8efc2ec1
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-28 15:31:44 -0500

    Add class to layout <body> to show controller name and action name. In main.js, check for body class before listening for support modal clicks & continuously running AJAX request for live poll results

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,27 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  var contactSupportLink = document.querySelector(".contact-support");
-  var contactModal = document.querySelector(".modal-container");
-  var closeModal = document.querySelector(".contact-close")
+  if (document.body.classList[0] == "sessions-new" || document.body.classList[0] == "events-show") {
+    var contactSupportLink = document.querySelector(".contact-support");
+    var contactModal = document.querySelector(".modal-container");
+    var closeModal = document.querySelector(".contact-close")
 
-  contactSupportLink.addEventListener('touchstart', function(e) {
-    e.preventDefault();
+    contactSupportLink.addEventListener('touchstart', function(e) {
+      e.preventDefault();
 
-    contactModal.style.display = "block";
+      contactModal.style.display = "block";
 
-    closeModal.addEventListener('click', function() {
-      contactModal.style.display = "none";
+      closeModal.addEventListener('click', function() {
+        contactModal.style.display = "none";
+      });
     });
-  });
 
-  contactSupportLink.addEventListener('click', function(e) {
-    e.preventDefault();
+    contactSupportLink.addEventListener('click', function(e) {
+      e.preventDefault();
 
-    contactModal.style.display = "unset";
+      contactModal.style.display = "unset";
 
-    closeModal.addEventListener('click', function() {
-      contactModal.style.display = "none";
+      closeModal.addEventListener('click', function() {
+        contactModal.style.display = "none";
+      });
     });
-  });
+  }
+
+  if (document.body.classList[0] == "session_details-index") {
+    setInterval(function() {
+      $.ajax({ url: 'https://core.eventmobi.com/cms/v1/events/liveresults/questions?event_id=21555&hash=d8fc7271bd60311e22fab6dbadd24cf2d72ba4cb1f583fe14f809bff4744245d&question_id=348574&question_type=live_poll&session_id=5d44d348-aa74-49bf-8466-1acd5573c038',
+      method: 'GET',
+      dataType: 'json'
+    }).done(function(data) {
+      console.log(data);
+    })
+  }, 500)
+  }
 
 });
