cba838ecff29888a32064008108036f90a7bb096
Cecilia Wong <ceciliakhwong@gmail.com>
2017-12-02 16:10:25 -0500

    If user is scheduling an announcement, change the confirmation message.

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   var scheduleCheckbox = document.querySelector('input[name="scheduled"]');
   var scheduleAlertOptions = document.querySelector('.schedule-alert-options');
+  var sendButton = document.querySelector('input[name="commit"]');
 
   console.log('alerts.js at werk');
 
@@ -9,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
   scheduleCheckbox.addEventListener('change', function() {
     if (this.checked) {
       scheduleAlertOptions.style.display = "unset";
+      sendButton.dataset.confirm = "You are about to schedule an announcement that will be sent to all users of the event app. Are you sure you want to schedule this announcement?";
     } else {
       scheduleAlertOptions.style.display = "none";
     }
